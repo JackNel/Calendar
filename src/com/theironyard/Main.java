d8f5dcea709ac53a8a4f7c67d1fc070dccaf4918
JackNel <jcneligan@gmail.com>
2015-11-04 12:37:51 -0500

    Add paging

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -11,6 +11,8 @@ import java.util.HashMap;
 
 public class Main {
 
+    static final int SHOW_COUNT = 5;
+
     public static void createTables(Connection conn) throws SQLException {
         Statement stmt = conn.createStatement();
         stmt.execute("CREATE TABLE IF NOT EXISTS events (id IDENTITY, description VARCHAR, start_date TIMESTAMP)");
@@ -23,9 +25,12 @@ public class Main {
         stmt.execute();
     }
 
-    public static ArrayList<Event> selectEvents(Connection conn) throws SQLException {
+    public static ArrayList<Event> selectEvents(Connection conn, boolean isAsc, int offset) throws SQLException {
         ArrayList<Event> events = new ArrayList();
-        PreparedStatement stmt = conn.prepareStatement("SELECT * FROM events");
+        String query = String.format("SELECT * FROM events ORDER BY start_date %s LIMIT ? OFFSET ?", isAsc ? "ASC" : "DESC");
+        PreparedStatement stmt = conn.prepareStatement(query);
+        stmt.setInt(1, SHOW_COUNT);
+        stmt.setInt(2, offset);
         ResultSet results = stmt.executeQuery();
         while (results.next()) {
             Event event = new Event();
@@ -37,6 +42,10 @@ public class Main {
         return events;
     }
 
+    public static ArrayList<Event> selectEvents(Connection conn) throws SQLException {
+        return selectEvents(conn, true, 0);
+    }
+
 
     public static void main(String[] args) throws SQLException {
         Connection conn = DriverManager.getConnection("jdbc:h2:./main");
@@ -45,9 +54,23 @@ public class Main {
         Spark.get(
                 "/",
                 ((request, response) -> {
+                    String isAscStr = request.queryParams("isAsc");
+                    boolean isAsc = isAscStr != null && isAscStr.equals("true");
+
+                    String offsetStr = request.queryParams("offset");
+                    int offset = 0;
+                    try {
+                        offset = Integer.valueOf(offsetStr);
+                    }
+                    catch (Exception e) {
+
+                    }
+
                     HashMap m = new HashMap();
                     m.put("now", LocalDateTime.now());
-                    m.put("events", selectEvents(conn));
+                    m.put("events", selectEvents(conn, isAsc, offset));
+                    m.put("isAsc", isAsc);
+                    m.put("nextOffset", offset + SHOW_COUNT);
                     return new ModelAndView(m, "events.html");
                 }),
                 new MustacheTemplateEngine()
@@ -62,8 +85,7 @@ public class Main {
                     try {
                         LocalDateTime startDate = LocalDateTime.parse(startDateStr);
                         insertEvent(conn, description, startDate);
-                    }
-                    catch (Exception e) {
+                    } catch (Exception e) {
 
                     }
                     response.redirect("/");
